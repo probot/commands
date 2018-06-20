484fd90d25a1238ad5beba53534f45b0922221c8
Brandon Keepers <bkeepers@github.com>
2018-06-19 22:34:00 -0500

    Alias variable name

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ class Command {
   }
 
   listener (context) {
-    const {comment, issue, pull_request} = context.payload
+    const {comment, issue, pull_request: pr} = context.payload
 
-    const command = (comment || issue || pull_request).body.match(this.matcher)
+    const command = (comment || issue || pr).body.match(this.matcher)
 
     if (command && this.name === command[1]) {
       return this.callback(context, {name: command[1], arguments: command[2]})
